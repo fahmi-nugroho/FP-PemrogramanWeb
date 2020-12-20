0587dd76ef71fbabe3d2b983793295b69c23f1f1
fahmi-nugroho <fahminugroho23@gmail.com>
2020-12-20 20:04:05 +0700

    progress 1 halaman index

--- assets/database/online_shop.sql
+++ assets/database/online_shop.sql
@@ -1,3 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 20, 2020 at 02:02 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fp_pemweb2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_web`
+--
 
 CREATE TABLE `admin_web` (
   `id_admin` int(3) NOT NULL,
@@ -6,16 +34,34 @@ CREATE TABLE `admin_web` (
   `nama` varchar(25) NOT NULL,
   `telepon` varchar(12) NOT NULL,
   `jabatan` varchar(10) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `categories`
+--
 
 CREATE TABLE `categories` (
   `id_kategori` int(4) NOT NULL,
   `nama` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `categories`
+--
 
+INSERT INTO `categories` (`id_kategori`, `nama`) VALUES
+(1, 'Game'),
+(2, 'Console Game'),
+(3, 'Aksesoris Game'),
+(4, 'Voucher Game');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `daftar_order`
+--
 
 CREATE TABLE `daftar_order` (
   `kode_order` varchar(25) NOT NULL,
@@ -23,8 +69,13 @@ CREATE TABLE `daftar_order` (
   `jam_order` time NOT NULL,
   `orders_info` text NOT NULL,
   `status` varchar(5) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `konfirmasi`
+--
 
 CREATE TABLE `konfirmasi` (
   `kode_order` varchar(50) NOT NULL,
@@ -33,19 +84,29 @@ CREATE TABLE `konfirmasi` (
   `nama_bank` varchar(50) NOT NULL,
   `jumlah_transfer` varchar(50) NOT NULL,
   `alamat_kirim` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news`
+--
 
 CREATE TABLE `news` (
-  `id` int(11) NOT NULL,
+  `id_news` int(11) NOT NULL,
   `judul` varchar(50) NOT NULL,
   `tanggal` datetime NOT NULL,
   `author` varchar(50) NOT NULL,
   `isi` text NOT NULL,
   `kategori` text NOT NULL,
   `views` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembeli`
+--
 
 CREATE TABLE `pembeli` (
   `kode_order` varchar(25) NOT NULL,
@@ -53,43 +114,168 @@ CREATE TABLE `pembeli` (
   `email_pembeli` varchar(25) NOT NULL,
   `telepon_pembeli` varchar(25) NOT NULL,
   `alamat_pembeli` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `produk`
+--
 
 CREATE TABLE `produk` (
   `id_produk` int(5) NOT NULL,
-  `category` varchar(20) NOT NULL,
+  `id_kategori` int(4) NOT NULL,
+  `penjual` varchar(50) NOT NULL,
   `nama_produk` varchar(50) NOT NULL,
   `deskripsi` text NOT NULL,
   `harga` double NOT NULL,
-  `stok` varchar(3) NOT NULL,
-  `views` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `stok` int(3) NOT NULL,
+  `gambar` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `produk`
+--
+
+INSERT INTO `produk` (`id_produk`, `id_kategori`, `penjual`, `nama_produk`, `deskripsi`, `harga`, `stok`, `gambar`) VALUES
+(1, 2, 'G Shop', 'Playstation 4', 'Playstation 4', 3000000, 20, 'produk1.png'),
+(2, 2, 'G Shop', 'Nintendo Switch', 'Nintendo Switch', 5000000, 10, 'produk2.png'),
+(3, 3, 'G Shop', 'Controller Nintendo Switch', 'Controller Nintendo Switch', 900000, 15, 'produk3.png'),
+(4, 1, 'G Shop', 'Zelda BOTW Nintendo Switch', 'Zelda BOTW Nintendo Switch', 500000, 5, 'produk4.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statistik`
+--
 
 CREATE TABLE `statistik` (
   `ip` varchar(20) NOT NULL,
   `tanggal` date NOT NULL,
   `hits` int(11) NOT NULL,
   `online` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `voucher`
+--
+
+CREATE TABLE `voucher` (
+  `id_voucher` int(11) NOT NULL,
+  `jenis_voucher` varchar(20) NOT NULL,
+  `gambar` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `voucher`
+--
+
+INSERT INTO `voucher` (`id_voucher`, `jenis_voucher`, `gambar`) VALUES
+(1, 'Playstation Plus', 'playstation1.png'),
+(2, 'Xbox Live', 'xbox1.png'),
+(3, 'Google Play', 'googleplay1.png'),
+(4, 'Steam', 'steam1.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_web`
+--
 ALTER TABLE `admin_web`
   ADD PRIMARY KEY (`id_admin`);
 
+--
+-- Indexes for table `categories`
+--
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id_kategori`);
 
+--
+-- Indexes for table `daftar_order`
+--
 ALTER TABLE `daftar_order`
   ADD KEY `kode_order` (`kode_order`);
 
+--
+-- Indexes for table `konfirmasi`
+--
 ALTER TABLE `konfirmasi`
   ADD PRIMARY KEY (`kode_order`);
 
+--
+-- Indexes for table `news`
+--
 ALTER TABLE `news`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_news`);
 
+--
+-- Indexes for table `pembeli`
+--
 ALTER TABLE `pembeli`
   ADD PRIMARY KEY (`kode_order`);
 
+--
+-- Indexes for table `produk`
+--
 ALTER TABLE `produk`
-  ADD PRIMARY KEY (`id_produk`);
+  ADD PRIMARY KEY (`id_produk`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indexes for table `voucher`
+--
+ALTER TABLE `voucher`
+  ADD PRIMARY KEY (`id_voucher`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_web`
+--
+ALTER TABLE `admin_web`
+  MODIFY `id_admin` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id_kategori` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `news`
+--
+ALTER TABLE `news`
+  MODIFY `id_news` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_produk` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `voucher`
+--
+ALTER TABLE `voucher`
+  MODIFY `id_voucher` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `produk`
+--
+ALTER TABLE `produk`
+  ADD CONSTRAINT `fk_kategori` FOREIGN KEY (`id_kategori`) REFERENCES `categories` (`id_kategori`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
